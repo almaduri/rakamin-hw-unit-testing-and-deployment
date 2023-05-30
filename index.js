af13ee1d363a8ab0b44505ffb862abff193f5823
almaduri <ali.mdrw@gmail.com>
2023-05-30 18:10:36 +0700

    add todo routes

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("tes")
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const pool = require('./db')
+app.get('/', async (req, res) => {
+  const todos = await pool.query('select * from todo')
+  res.json(todos.rows)
+})
+
+app.listen(3000)
